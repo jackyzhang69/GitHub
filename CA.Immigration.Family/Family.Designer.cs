2edbac71ec18afec3199292d7964b3469b9a05c6
Jacky Zhang <noah.consultant@outlook.com>
2016-02-07 10:58:30 -0800

    Initial commit for solution frame work

---
+++ CA.Immigration.Family/Family.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CA.Immigration.Family
+{
+    partial class Family
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Family
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1408, 817);
+            this.Name = "Family";
+            this.Text = "Family Sponsorship Advisor & Processing";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
